750bb6ec6b171199594b8fa2e439ae7f457791db
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-05 17:27:58 -0300

    Tela Sobre

--- src/gui/MainTelaPrincipalController.java
+++ src/gui/MainTelaPrincipalController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainTelaPrincipalController implements Initializable {
 
@@ -28,12 +37,30 @@ public class MainTelaPrincipalController implements Initializable {
 
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("onMenuItemSobreAction()");
+		carregarTela("/gui/Sobre.fxml");
 	}
 
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 
+	}
+
+	public synchronized void carregarTela(String enderecoCompleto) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(enderecoCompleto));
+			VBox vBox = loader.load();
+			 
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox)((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(vBox);
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IoException", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
