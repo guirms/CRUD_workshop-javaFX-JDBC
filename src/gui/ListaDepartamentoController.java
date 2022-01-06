a81fcc05a02be4c51ce72fd223202c2295b2c726
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-06 13:13:45 -0300

    DepartamentoForm

--- src/gui/ListaDepartamentoController.java
+++ src/gui/ListaDepartamentoController.java
@@ -1,18 +1,26 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import gui.util.Alerts;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entidades.Departamento;
 import model.services.DepartamentoService;
@@ -41,8 +49,9 @@ public class ListaDepartamentoController implements Initializable {
 	}
 	
 	@FXML
-	public void onBtNovoAction() {
-		System.out.println("Botão novo");
+	public void onBtNovoAction(ActionEvent evento) {
+		Stage parentStage = Utils.stageAtual(evento);
+		criarDialogoForm("/gui/DepartamentoForm.fxml", parentStage);
 	}
 	
 	@Override
@@ -65,6 +74,24 @@ public class ListaDepartamentoController implements Initializable {
 		List<Departamento> lista = service.encontrarTudo();
 		obsList = FXCollections.observableArrayList(lista);
 		tableViewDepartamento.setItems(obsList);
+	}
+	
+	private void criarDialogoForm(String enderecoCompleto, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(enderecoCompleto));
+			Pane pane = loader.load();
+			
+			Stage stageDialogo = new Stage();
+			stageDialogo.setTitle("Digite os dados do departamento");
+			stageDialogo.setScene(new Scene(pane));
+			stageDialogo.setResizable(false);
+			stageDialogo.initOwner(parentStage);
+			stageDialogo.initModality(Modality.WINDOW_MODAL);
+			stageDialogo.showAndWait();
+			
+		} catch (IOException io){
+			Alerts.showAlert("IOException", "Erro ao carregar a tela", io.getMessage(), AlertType.ERROR);
+		}
 	}
 	
 }
---
+++ src/gui/Utils.java
@@ -0,0 +1,14 @@
+package gui;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+
+	public static Stage stageAtual (ActionEvent evento) {
+		return (Stage) ((Node) evento.getSource()).getScene().getWindow();
+	}
+	
+	
+}
