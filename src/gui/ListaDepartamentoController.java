6a66059f49b321faf039e699e1072be24209bda3
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-07 15:44:48 -0300

    Deletando pasta .settings

--- src/gui/ListaDepartamentoController.java
+++ src/gui/ListaDepartamentoController.java
@@ -8,7 +8,6 @@ import java.util.ResourceBundle;
 
 import application.Main;
 import db.DbException;
-import db.DbIntegrityException;
 import gui.ouvintes.OuvinteDeMudancaDeDados;
 import gui.util.Alerts;
 import gui.util.Utils;
