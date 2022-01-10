17e2c4297d03c20c1c071e3245795483404a3f9e
Guilherme Santana <guilhermesantana84@hotmail.com>
2022-01-09 23:59:33 -0300

    Finalização do projeto (sem utilidade)

--- src/aplicacao/Main.java
+++ src/aplicacao/Main.java
@@ -1,4 +1,4 @@
-package application;
+package aplicacao;
 
 import java.io.IOException;
 
@@ -23,7 +23,7 @@ public class Main extends Application {
 			
 			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.setTitle("Sistema de manipulação de dados");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/gui/ListaDepartamentoController.java
+++ src/gui/ListaDepartamentoController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.ResourceBundle;
 
-import application.Main;
+import aplicacao.Main;
 import db.DbException;
 import gui.ouvintes.OuvinteDeMudancaDeDados;
 import gui.util.Alerts;
